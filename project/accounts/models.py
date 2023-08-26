559813b8eee220554d85af96faf228b38874e8ad
Sham-punka <130845477+Sham-punka@users.noreply.github.com>
2023-08-26 22:20:03 +0300

    Delete project directory

--- project/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Profile(models.Model):
-    code_of_confirm = models.CharField(max_length=3, default=None)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return str(self.code_of_confirm)
--- project/game/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Post, Response
-
-admin.site.register(Post)
-admin.site.register(Response)
